3112d1a9ceb9c2b0d7f6cac443ea612e6e8b8ac7
Fréderic NIVOSON <fredynivoson@gmail.com>
2024-01-23 09:54:40 +0300

    feat: Manage application port through server.js and add start script inside package.json

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const normalizePort = (specifiedPort) => {
+	const port = parseInt(specifiedPort, 10);
+	if (isNaN(port)) {
+		return specifiedPort;
+	}
+	if (port >= 0) {
+		return port;
+	}
+	return false;
+};
+
+const port = normalizePort(process.env.PORT || '5000');
+
+app.listen(port, () => {
+	console.log('App listen on port ' + port);
+})
