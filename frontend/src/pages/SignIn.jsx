8048bae06766e746557a6563de72d364f94879d1
Fréderic NIVOSON <fredynivoson@gmail.com>
2024-01-24 10:01:07 +0300

    feat: Create page structures and their routes

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const SignIn = () => (
+	<h1>Sign in page</h1>
+)
+
+export default SignIn
---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,3 @@
+export {default as Login} from './SignIn.jsx'
+export {default as SignUp} from './SignUp.jsx'
+export {default as Home} from './Home.jsx'
